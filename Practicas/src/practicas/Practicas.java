9201070ae8383d58e1f50d0cb17f92608fc6649e
joanayala <jflaco85@gmail.com>
2020-03-24 14:39:39 -0500

    Codigo impresión de nros del 1 al 10

--- Practicas/src/practicas/Practicas.java
+++ Practicas/src/practicas/Practicas.java
@@ -16,6 +16,18 @@ public class Practicas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //CREE UN SCRIPT QUE PERMITA VISUALIZAR LOS NÚMEROS 
+        //DEL 1 AL 10 CON CICLO FOR INICIANDO LA VARIABLE INCREMENTO EN 0
+        int i=0;
+        for(i=0;i<10;i++){
+            System.out.println(i+1);
+        }
+        System.out.println("#######################################");
+        //CREE UN SCRIPT QUE PERMITA VISUALIZAR LOS NÚMEROS 
+        //DEL 1 AL 10 CON CICLO FOR INICIANDO LA VARIABLE INCREMENTO EN 1
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+        }
     }
     
 }
