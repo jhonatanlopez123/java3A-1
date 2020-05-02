79ed5be5038358d9ba9f396da13705a4477912d7
joanayala <jflaco85@gmail.com>
2020-05-02 13:49:12 -0500

    Load Market Project: Regiter new products

--- Market/src/market/Market.java
+++ Market/src/market/Market.java
@@ -5,8 +5,6 @@
  */
 package market;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author Family
@@ -17,35 +15,7 @@ public class Market {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        //Products products = new Products();
-        //products.setVisible(true);
-        
         // TODO code application logic here
-        //ArrayList<String> cars = new ArrayList<String>();
-        //cars.add("Chevrolet"); //0
-        //cars.add("Mazda");  //1
-        //cars.add("Nissan"); //2
-        //cars.add("Suzuki"); //3
-        //cars.add("Yamaha"); //4
-        
-        //System.out.println("Object: " + cars);
-        
-        //for (int i=0;i<cars.size();i++){
-            //System.out.println(cars.get(i));
-        //}
-        
-        /*System.out.println(cars.get(2));
-        String old_car = cars.get(2);
-        cars.set(2, "Nissan March");
-        System.out.println(old_car + " is now: " + cars.get(2));
-        cars.remove(1);
-        System.out.println(cars.get(1));
-        System.out.println("Size cars object: " + cars.size());
-        
-        System.out.println(cars);
-        cars.clear();
-        System.out.println(cars);*/
     }
     
 }
